29ce2f57872ea0d0728fb50f191ab5100dc57c8d
hubnotch <love2ekene@gmail.com>
2021-12-11 04:29:31 +0100

    added the fontawesome icons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+    faLinkedin
+} from "@fortawesome/free-brands-svg-icons";
 import "../App.css";
 
 function Button (){
     return (
         <div className="button">
             <button type="button" className="btn email"><FontAwesomeIcon icon={faEnvelope}/> Email</button>
-            <button type="button" className="btn linkedin"> LinkedIn</button>
+            <button type="button" className="btn linkedin"><FontAwesomeIcon icon={faLinkedin}/> LinkedIn</button>
         </div>
     )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faCoffee, faTwitter } from "@fortawesome/free-solid-svg-icons";
-import "../App.css"
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitterSquare,
+  faInstagramSquare,
+  faGithubSquare,
+  faFacebookSquare
+} from "@fortawesome/free-brands-svg-icons";
+import "../App.css";
 
 
 function Footer() {
-    return (
-      <div className="footer">
-        <FontAwesomeIcon icon="coffee" />
-        <FontAwesomeIcon icon={["fas", "coffee"]} />
-        <FontAwesomeIcon icon={["far", "twitter"]} />
-        <FontAwesomeIcon icon={faCoffee} />
-        {/* <FontAwesomeIcon icon={FaTwitterSquare} /> */}
-        <FontAwesomeIcon icon={faCoffee} />
-        <FontAwesomeIcon icon={faCoffee} />
-        <FontAwesomeIcon icon={faCoffee} />
-      </div>
-    );
+  return (
+    <div className="footer">
+      <FontAwesomeIcon icon={faTwitterSquare} size="2x" className="icons"/>
+      <FontAwesomeIcon icon={faFacebookSquare} size="2x" className="icons" />
+      <FontAwesomeIcon icon={faInstagramSquare} size="2x" className="icons" />
+      <FontAwesomeIcon icon={faGithubSquare} size="2x" className="icons" />
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
