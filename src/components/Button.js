33337c7341a71ab65c7ca614cbea84d4d498842e
hubnotch <love2ekene@gmail.com>
2021-12-21 23:37:05 +0100

    added links to email and linkedin

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,33 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import {
-    faLinkedin
-} from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import "../App.css";
 
-function Button (){
-    return (
-        <div className="button">
-            <button type="button" className="btn email"><FontAwesomeIcon icon={faEnvelope}/> Email</button>
-            <button type="button" className="btn linkedin"><FontAwesomeIcon icon={faLinkedin}/> LinkedIn</button>
-        </div>
-    )
+function Button() {
+  return (
+    <div className="button">
+      <a
+        href="mailto:love2ekene@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button type="button" className="btn email">
+          <FontAwesomeIcon icon={faEnvelope} /> Email
+        </button>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/ekene-ugwuanyi/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button type="button" className="btn linkedin">
+          <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+        </button>
+      </a>
+    </div>
+  );
 }
 
-export default Button
+export default Button;
