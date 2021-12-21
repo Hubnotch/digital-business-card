397ddbd200629eecc8b81e91af9f94d9f28abf13
hubnotch <love2ekene@gmail.com>
2021-12-21 23:08:35 +0100

    added links to social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,18 +4,42 @@ import {
   faTwitterSquare,
   faInstagramSquare,
   faGithubSquare,
-  faFacebookSquare
+  faFacebookSquare,
 } from "@fortawesome/free-brands-svg-icons";
 import "../App.css";
-
 
 function Footer() {
   return (
     <div className="footer">
-      <FontAwesomeIcon icon={faTwitterSquare} size="2x" className="icons"/>
-      <FontAwesomeIcon icon={faFacebookSquare} size="2x" className="icons" />
-      <FontAwesomeIcon icon={faInstagramSquare} size="2x" className="icons" />
-      <FontAwesomeIcon icon={faGithubSquare} size="2x" className="icons" />
+      <a
+        href="https://twitter.com/hubnotch"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faTwitterSquare} size="2x" className="icons" />
+      </a>
+
+      <a
+        href="https://www.facebook.com/ugwuanyi.l.ekene"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faFacebookSquare} size="2x" className="icons" />
+      </a>
+      <a
+        href="https://www.instagram.com/lillegenius/?hl=en"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faInstagramSquare} size="2x" className="icons" />
+      </a>
+      <a
+        href="https://github.com/Hubnotch"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faGithubSquare} size="2x" className="icons" />
+      </a>
     </div>
   );
 }
